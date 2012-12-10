58f7b86139e77c7a86351b091d3705fe4447371c
alexbirukov <lord_maloy@mail.ru>
2012-12-10 23:31:03 +0600

    Add Russian language
    Add Russian language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,81 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2012-09-27'
+	);
+
+	/**
+	 * Text Box
+	 */
+	$dictionary = array(
+
+		'Single Line' => 
+		'Одна строка',
+
+		'Small Box' => 
+		'Маленькое поле ',
+
+		'Medium Box' => 
+		'Среднее поле',
+
+		'Large Box' => 
+		'Большое поле',
+
+		'Huge Box' => 
+		'Огромное поле',
+
+		'Limit input to %s characters' => 
+		'Лимит вводимых знаков %s',
+
+		'Show %s characters in preview' => 
+		'Отображать %s знаков в предпросмотре',
+
+		'%s Output with handles' => 
+		'%s Выводить как обработчик',
+
+		'%s Output as CDATA' => 
+		'%s Выводить как CDATA',
+
+		'$1 of $2 remaining' => 
+		'Осталось $1 из $2',
+
+		'Find values that match the given query. Can use operators <code>and</code> and <code>not</code>.' => 
+		'Найти значения, которые соответствуют данному запросу. Можно использовать операторы <code>and</code> и <code>not</code>.',
+
+		'Find values that do not match the given query. Can use operators <code>and</code> and <code>not</code>.' => 
+		'Найти значения, которые не соответствуют данному запросу. Можно использовать операторы <code>and</code> и <code>not</code>.',
+
+		'Find values that match the given <a href="%s">MySQL regular expressions</a>.' => 
+		'Найти значения, которые соответствуют данному <a href="%s">регулярному выражению MySQL</a>.',
+
+		'Find values that do not match the given <a href="%s">MySQL regular expressions</a>.' => 
+		'Найти значения, которые не соответствуют данному <a href="%s">регулярному выражению MySQL</a>.',
+
+		'Find values that contain the given string.' => 
+		'Найти значения, которые содержат заданную строку.',
+
+		'Find values that do not contain the given string.' => 
+		'Найти значения, которые не содержат заданную строку.',
+
+		'Find values that start with the given string.' => 
+		'Найти значения, которые начинаются с заданной строки.',
+
+		'Find values that do not start with the given string.' => 
+		'Найти значения, которые не начинаются с заданной строки.',
+
+		'Find values that end with the given string.' => 
+		'Найти значения, которые заканчиваются заданной строкой.',
+
+		'Find values that do not end with the given string.' => 
+		'Найти значения, которые не заканчиваются заданной строкой.',
+
+		'Find values that are an exact match for the given string.' => 
+		'Найти значения с точным совпадением.',
+
+	);
